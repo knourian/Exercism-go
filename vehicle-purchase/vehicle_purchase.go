73641a10ce0468e4fa06a40451a7117fb0116b17
Khashayar Nourian <knourianchali@gmail.com>
2023-03-15 07:27:04 +0330

    Vehicle Purchase solution

--- vehicle-purchase/vehicle_purchase.go
+++ vehicle-purchase/vehicle_purchase.go
@@ -1,16 +1,33 @@
 package purchase
 
+import (
+	"fmt"
+)
+
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	panic("NeedsLicense not implemented")
+	if kind == "car" || kind == "truck" {
+		return true
+	}
+	return false
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	panic("ChooseVehicle not implemented")
+	format := "%s is clearly the better choice."
+	if option1 < option2 {
+		return fmt.Sprintf(format, option1)
+	}
+	return fmt.Sprintf(format, option2)
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
 func CalculateResellPrice(originalPrice, age float64) float64 {
-	panic("CalculateResellPrice not implemented")
+	if age < 3 {
+		return originalPrice * 0.8
+	} else if age < 10 {
+		return originalPrice * 0.7
+	} else {
+		return originalPrice * 0.5
+	}
 }
