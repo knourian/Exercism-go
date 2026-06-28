d5141f647d42faade159ea862bbf7f1fc1d6c007
khashayar nourian <knourian.dev@gmail.com>
2026-06-28 17:41:09 +0330

    hamming solution

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,5 +1,17 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	if len(a) != len(b) {
+		return 0, errors.New("strands must be of equal length")
+	}
+
+	distance := 0
+	for index := range a {
+		if a[index] != b[index] {
+			distance++
+		}
+	}
+	return distance, nil
 }
