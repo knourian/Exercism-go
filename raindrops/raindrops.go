7d306d6da6b28afa51ed71771989f36cf18828ac
khashayar nourian <knourian.dev@gmail.com>
2026-06-28 17:52:05 +0330

    Raindrops solution

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,5 +1,22 @@
 package raindrops
 
+import "fmt"
+
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	output := ""
+
+	if number%3 == 0 {
+		output += "Pling"
+	}
+	if number%5 == 0 {
+		output += "Plang"
+	}
+	if number%7 == 0 {
+		output += "Plong"
+	}
+
+	if output == "" {
+		return fmt.Sprintf("%d", number)
+	}
+	return output
 }
